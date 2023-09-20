632e47a13214f71842d0637820e24c1244254bf1
Miguel Angel Cruz Vargas <isc20350280@gmail.com>
2023-09-20 17:00:22 -0600

    query params

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require("express")
 
 const app = express()
 
-app.get("/:user/:password",(req,res) => {
+//==================== METODO GET ========================
+app.get('/', (req,res) => { res.json({msg:'hello get'})})
+
+//======================= ENDPOINT ============================
+
+//paramns, query y patch
+//uso de la destructuracion
+app.get("/:user/:password",(req,res) => {//ednpoint es el conjuto del metodo http, la ruta 
     const {user,password} = req.params
     if(user == 'Miguel' && password == '123'){
         res.json({msg:'Inicio de sesion exitoso'})
@@ -10,12 +17,37 @@ app.get("/:user/:password",(req,res) => {
     }
         res.json({msg:'Fallo en el usuario y contraseña'})
     })
+//==========================QUERY=================================
+
+//http://localhost:3000/login?fullname=miguel%20angel%20cruz%20vargas
+//http://localhost:3000/login?user=miguel&pws=123
+
+app.get('/login' ,(req , res) =>{const{user,password}=req.query 
+if(!user|| !password){
+    res.json({msg:'You need to provide <user> and <password> params'})
+}  
+
+if(user == 'Miguel' && password =='123'){
+    res.json({msg: 'Sing in succesfull'})
+    return
+}
+res.status(404).json({msg:'wrong user or password'})
+
+})
+
+//=======================METODO POST =====================================
 
 app.post("/",(req,res) => {
     res.json({
         msg:'Hello Post'
     })
 })
+
+
+
+
+
+
 
 
 app.put("/",(req,res) =>{
@@ -36,6 +68,8 @@ app.delete("/",(req,res) =>{
     })
 })
 
+
+  
 /*
 funcionalidad
 */
